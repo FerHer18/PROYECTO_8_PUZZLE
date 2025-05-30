466ce5f1aaf75bf7a3b0e24c51a31c882d33bfe7
FerHer18 <fernandogabrielhernandezj@gmail.com>
2025-05-30 10:46:53 -0600

    Automatico

--- src/main/java/com/mycompany/proyecto_final_2_0/INICIO_AUTO.java
+++ src/main/java/com/mycompany/proyecto_final_2_0/INICIO_AUTO.java
@@ -4,9 +4,13 @@
  */
 package com.mycompany.proyecto_final_2_0;
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.List;
 
 public class INICIO_AUTO extends javax.swing.JFrame {
     private JFrame anterior;
+    
     
     public INICIO_AUTO(JFrame anterior) {
         initComponents();
@@ -33,59 +37,179 @@ public class INICIO_AUTO extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
-        jLabel2 = new javax.swing.JLabel();
-        jLabel3 = new javax.swing.JLabel();
-        jButton1 = new javax.swing.JButton();
-        fondo = new javax.swing.JLabel();
+        jLabel9 = new javax.swing.JLabel();
+        jButton2 = new javax.swing.JButton();
+        jLabel10 = new javax.swing.JLabel();
+        jLabel11 = new javax.swing.JLabel();
+        jLabel12 = new javax.swing.JLabel();
+        jComboBox2 = new javax.swing.JComboBox<>();
+        label5 = new java.awt.Label();
+        jLabel13 = new javax.swing.JLabel();
+        label6 = new java.awt.Label();
+        jLabel14 = new javax.swing.JLabel();
+        jLabel15 = new javax.swing.JLabel();
+        jLabel16 = new javax.swing.JLabel();
+        label7 = new java.awt.Label();
+        jLabel17 = new javax.swing.JLabel();
+        jButton3 = new javax.swing.JButton();
+        label8 = new java.awt.Label();
+        jSeparator1 = new javax.swing.JSeparator();
+        fondo1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setPreferredSize(new java.awt.Dimension(905, 553));
         setSize(new java.awt.Dimension(1260, 700));
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         jPanel1.setPreferredSize(new java.awt.Dimension(905, 553));
         jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jLabel2.setText("punto inicio");
-        jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(210, 150, -1, -1));
+        jLabel9.setBackground(new java.awt.Color(109, 84, 34));
+        jLabel9.setFont(new java.awt.Font("Cambria", 1, 36)); // NOI18N
+        jLabel9.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel9.setText("          BIENVENIDO A JUEGO AUTOMATICO");
+        jLabel9.setOpaque(true);
+        jPanel1.add(jLabel9, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 60, 740, 50));
 
-        jLabel3.setText("META");
-        jPanel1.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(590, 150, -1, -1));
-
-        jButton1.setText("JUGAR");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        jButton2.setBackground(new java.awt.Color(109, 84, 34));
+        jButton2.setFont(new java.awt.Font("Candara", 1, 18)); // NOI18N
+        jButton2.setForeground(new java.awt.Color(255, 255, 255));
+        jButton2.setText("JUGAR");
+        jButton2.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                jButton2ActionPerformed(evt);
             }
         });
-        jPanel1.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(380, 290, 160, 70));
-        jPanel1.add(fondo, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 920, 550));
+        jPanel1.add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(490, 430, 170, 50));
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 920, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-        );
+        jLabel10.setBackground(new java.awt.Color(109, 84, 34));
+        jLabel10.setFont(new java.awt.Font("Cambria", 1, 36)); // NOI18N
+        jLabel10.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel10.setText("          BIENVENIDO A JUEGO AUTOMATICO");
+        jLabel10.setOpaque(true);
+        jPanel1.add(jLabel10, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 60, 740, 50));
+
+        jLabel11.setIcon(new javax.swing.ImageIcon(getClass().getResource("/caracol.png"))); // NOI18N
+        jLabel11.setText("jLabel3");
+        jPanel1.add(jLabel11, new org.netbeans.lib.awtextra.AbsoluteConstraints(480, 140, 160, 140));
+
+        jLabel12.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imposible.png"))); // NOI18N
+        jLabel12.setText("jLabel3");
+        jPanel1.add(jLabel12, new org.netbeans.lib.awtextra.AbsoluteConstraints(680, 140, 160, 140));
+
+        jComboBox2.setBackground(new java.awt.Color(204, 153, 0));
+        jComboBox2.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "VERTICAL", "HORIZONTAL", "CARACOL", "IMPOSIBLE" }));
+        jComboBox2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jComboBox2ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(jComboBox2, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 430, 170, 50));
+
+        label5.setBackground(new java.awt.Color(255, 255, 255));
+        label5.setFont(new java.awt.Font("Cambria", 1, 12)); // NOI18N
+        label5.setText("VERTICAL");
+        jPanel1.add(label5, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 290, -1, -1));
+
+        jLabel13.setIcon(new javax.swing.ImageIcon(getClass().getResource("/horizontal.png"))); // NOI18N
+        jLabel13.setText("jLabel3");
+        jPanel1.add(jLabel13, new org.netbeans.lib.awtextra.AbsoluteConstraints(290, 140, 160, 140));
+
+        label6.setBackground(new java.awt.Color(255, 255, 255));
+        label6.setFont(new java.awt.Font("Cambria", 1, 12)); // NOI18N
+        label6.setText("CARACOL");
+        jPanel1.add(label6, new org.netbeans.lib.awtextra.AbsoluteConstraints(530, 290, -1, -1));
+
+        jLabel14.setIcon(new javax.swing.ImageIcon(getClass().getResource("/vertical.png"))); // NOI18N
+        jLabel14.setText("jLabel3");
+        jPanel1.add(jLabel14, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 140, 160, 140));
+
+        jLabel15.setIcon(new javax.swing.ImageIcon(getClass().getResource("/vertical.png"))); // NOI18N
+        jLabel15.setText("jLabel3");
+        jPanel1.add(jLabel15, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 140, 160, 140));
+
+        jLabel16.setIcon(new javax.swing.ImageIcon(getClass().getResource("/vertical.png"))); // NOI18N
+        jLabel16.setText("jLabel3");
+        jPanel1.add(jLabel16, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 140, 160, 140));
+
+        label7.setBackground(new java.awt.Color(255, 255, 255));
+        label7.setFont(new java.awt.Font("Cambria", 1, 12)); // NOI18N
+        label7.setText("IMPOSIBLE");
+        jPanel1.add(label7, new org.netbeans.lib.awtextra.AbsoluteConstraints(720, 290, -1, -1));
+
+        jLabel17.setBackground(new java.awt.Color(109, 84, 34));
+        jLabel17.setFont(new java.awt.Font("Cambria", 1, 18)); // NOI18N
+        jLabel17.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel17.setText("         ELIGE LA META ");
+        jLabel17.setOpaque(true);
+        jPanel1.add(jLabel17, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 380, 220, 30));
+
+        jButton3.setBackground(new java.awt.Color(109, 84, 34));
+        jButton3.setFont(new java.awt.Font("Candara", 1, 18)); // NOI18N
+        jButton3.setText("REGRESAR");
+        jButton3.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(jButton3, new org.netbeans.lib.awtextra.AbsoluteConstraints(680, 430, 160, 50));
+
+        label8.setBackground(new java.awt.Color(255, 255, 255));
+        label8.setFont(new java.awt.Font("Cambria", 1, 12)); // NOI18N
+        label8.setText("HORIZONTAL");
+        jPanel1.add(label8, new org.netbeans.lib.awtextra.AbsoluteConstraints(330, 290, -1, -1));
+        jPanel1.add(jSeparator1, new org.netbeans.lib.awtextra.AbsoluteConstraints(970, 270, -1, -1));
+
+        fondo1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Fondo_inicio_auto.jpg"))); // NOI18N
+        jPanel1.add(fondo1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 920, 550));
+
+        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, 550));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        JUEGO_AUTO obj = new JUEGO_AUTO(this);
-        obj.setVisible(true);
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+
+    }//GEN-LAST:event_jButton3ActionPerformed
+
+    private void jComboBox2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jComboBox2ActionPerformed
+        jComboBox2.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "VERTICAL", "HORIZONTAL", "CARACOL", "IMPOSIBLE" }));
+    }//GEN-LAST:event_jComboBox2ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+         String seleccion = jComboBox2.getSelectedItem().toString();
+        int[][] estadoInicial = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 0, 8}
+        };
+        JUEGO_AUTO juego = new JUEGO_AUTO(estadoInicial, null, seleccion);
+        juego.setVisible(true);
         this.setVisible(false);
-    }//GEN-LAST:event_jButton1ActionPerformed
+    }//GEN-LAST:event_jButton2ActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel fondo;
-    private javax.swing.JButton jButton1;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel fondo1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JComboBox<String> jComboBox2;
+    private javax.swing.JLabel jLabel10;
+    private javax.swing.JLabel jLabel11;
+    private javax.swing.JLabel jLabel12;
+    private javax.swing.JLabel jLabel13;
+    private javax.swing.JLabel jLabel14;
+    private javax.swing.JLabel jLabel15;
+    private javax.swing.JLabel jLabel16;
+    private javax.swing.JLabel jLabel17;
+    private javax.swing.JLabel jLabel9;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JSeparator jSeparator1;
+    private java.awt.Label label5;
+    private java.awt.Label label6;
+    private java.awt.Label label7;
+    private java.awt.Label label8;
     // End of variables declaration//GEN-END:variables
 }
